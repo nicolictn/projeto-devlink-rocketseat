738603ff8a2efe1606061b9ac5cc4701dd8bbd4c
Nicoli Caetano <nicolictn@gmail.com>
2023-07-11 12:11:50 -0300

    add readme

---
+++ .vscode/script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //outra forma de se fazer:
+  //  if(body.constais('light')) {
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contais("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("scr", "./assets/avatar.png")
+  }
+}
